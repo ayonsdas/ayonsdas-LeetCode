b161a9e716e1c1f42708fa202ef2536f869b985f
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-22 17:51:17 -0500

    Delete 246-strobogrammatic-number directory

--- 246-strobogrammatic-number/246-strobogrammatic-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isStrobogrammatic(self, num: str) -> bool:
-        nums = {"6": "9", "1": "1", "9": "6", "8": "8", "0": "0"}
-        for i in range(len(num) // 2 + 1):
-            if not num[i] in nums:
-                return False
-            if num[~i] != nums[num[i]]:
-                return False
-        return True
