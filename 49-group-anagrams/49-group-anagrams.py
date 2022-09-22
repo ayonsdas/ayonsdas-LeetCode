fe604929ea7c0a79f869b06aa342adf2ca52d20f
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-22 18:29:38 -0500

    Time: 279 ms (9.42%), Space: 17.5 MB (74.14%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,8 +2,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         keys = {}
         for st in strs:
-            x = "".join(sorted([char for char in st]))
+            x = str(sorted([char for char in st]))
             if not x in keys:
                 keys[x] = []
             keys[x].append(st)
-        return list(keys.values())
+        l = []
+        for key in keys:
+            l.append(keys[key])
+        return l
