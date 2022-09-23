5a16fc0643281a97aa1ab7e47c61a7f7013e5c18
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-23 14:03:59 -0500

    Delete 1447-simplified-fractions directory

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def simplifiedFractions(self, n: int) -> List[str]:
-        x = []
-        for j in range(2, n + 1):
-            for i in range(1, j):
-                if math.gcd(i, j) == 1 or i == 1:
-                    x.append(str(i) + "/" + str(j))
-        return x
