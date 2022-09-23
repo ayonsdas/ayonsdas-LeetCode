0c8f3f7338e10383d29a082bd1c935c0cf5f21fc
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-23 17:19:07 -0500

    Time: 2225 ms (78.72%), Space: 28.4 MB (76.35%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -4,30 +4,12 @@ class MinHeap:
     
     def __init__(self):
         self.heap = []
-        
-    def parent(self, i: int) -> int:
-        return (i - 1) // 2
     
     def addVal(self, val: int):
         heappush(self.heap, val)
     
-    def updateKey(self, k: int, nVal: int) -> int:
-        oVal = self.heap[k]
-        self.heap[k] = new_val
-        while k > 0 and self.heap[self.parent(k)] > self.heap[k]:
-            self.heap[self.parent(k)], self.heap[k] = self.heap[k], self.heap[self.parent(k)]
-            k = self.parent(k)
-        return oVal
-    
     def poll(self) -> int:
         return heappop(self.heap)
-    
-    def deleteKey(self, k) -> int:
-        self.updateKey(k, -float("inf"))
-        return self.poll()
-    
-    def peek(self) -> int:
-        return self.heap[0]
 
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
