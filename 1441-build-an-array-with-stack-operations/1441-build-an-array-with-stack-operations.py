e07ed874268badca5dbd453db00cf636c9d42cc8
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-23 14:14:09 -0500

    Delete 1441-build-an-array-with-stack-operations directory

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def buildArray(self, target: List[int], n: int) -> List[str]:
-        t = []
-        x = Counter(target)
-        for i in range(1, min(target[-1], n) + 1):
-            t.append("Push")
-            if not i in x:
-                t.append("Pop")
-        return t
