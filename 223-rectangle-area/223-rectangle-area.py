b58c25edc43b2230b57f6bb44ed37f2b077ea573
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-23 14:33:34 -0500

    Delete 223-rectangle-area directory

--- 223-rectangle-area/223-rectangle-area.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        a = (min(ay2, by2) - max(ay1, by1)) * (min(ax2, bx2) - max(ax1, bx1))
-        if ax1 >= bx2 or bx1 >= ax2 or ay1 >= by2 or by1 >= ay2:
-            a = 0
-        return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1) - a
