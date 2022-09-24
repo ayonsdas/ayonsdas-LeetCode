0ed49dda1204b6913ab0e2f334754c540b7c2a7d
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-23 22:05:37 -0500

    Delete 1556-thousand-separator directory

--- 1556-thousand-separator/1556-thousand-separator.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def thousandSeparator(self, n: int) -> str:
-        t = ""
-        n = str(n)
-        for i in range(len(n)):
-            if i % 3 == 0 and i != 0:
-                t += "."
-            t += n[~i]
-        return t[::-1]
