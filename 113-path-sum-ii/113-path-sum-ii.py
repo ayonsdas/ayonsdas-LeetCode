5e39f6fc66879163ab71f785347b15dc2328cce7
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-23 21:26:18 -0500

    Time: 94 ms (19.95%), Space: 15.5 MB (89.27%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -1,27 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        return self.finder(root, [], 0, targetSum)
+
+    def dfs(self, root, path, ans, remainingSum):
+
+        if not root:
+            return
+
+        path.append(root.val)
+
+        if not root.left and not root.right and remainingSum == root.val:
+            ans.append(list(path))
+
+        self.dfs(root.left, path, ans, remainingSum - root.val)
+
+        self.dfs(root.right, path, ans, remainingSum - root.val)
+
+        path.pop()
     
-    def finder(self, root: Optional[TreeNode], currPath: List[int], currSum: int, targetSum: int) -> List[List[int]]:
-        if root == None:
-            return []
-        t = []
-        x = []
-        for l in currPath:
-            x.append(l)
-        x.append(root.val)
-        if currSum + root.val == targetSum and root.left == None and root.right == None:
-            
-            t.append(x)
-            return t
-        for z in self.finder(root.left, x, currSum + root.val, targetSum):
-            t.append(z)
-        for z in self.finder(root.right, x, currSum + root.val, targetSum):
-            t.append(z)
-        return t
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        ans = []
+        self.dfs(root, [], ans, targetSum)
+        return ans
