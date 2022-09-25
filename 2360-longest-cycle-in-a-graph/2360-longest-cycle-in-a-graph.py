59b385850f516f1ef6ee82258a47de9d705c2568
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-24 23:21:00 -0500

    Time: 3092 ms (13.06%), Space: 34.6 MB (81.11%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,31 +1,57 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
-        visited = set()
-        cycs = [0 for _ in range(len(edges))]
-        for i in range(len(edges)):
-            j = 1
-            t = i
-            while cycs[t] == 0:
-                if edges[t] == -1:
-                    for x in visited:
-                        cycs[x] = -1
-                    cycs[t] = -1
-                    break
-                cycs[t] = ~j
-                visited.add(t)
-                t = edges[t]
-                j += 1
-            else:
-                if cycs[t] == -1:
-                    for x in visited:
-                        cycs[x] = -1
-                elif cycs[t] < 0:
-                    for x in visited:
-                        if x != t:
-                            cycs[x] = j - ~cycs[t]
-                    cycs[t] = j - ~cycs[t]
-                else:
-                    for x in visited:
-                        cycs[x] = cycs[t]
-            visited = set()
-        return max(cycs)
+        length = len(edges) 
+        candidates = {node for node in range(length)}
+        dq = collections.deque([]) 
+        indegree = [0 for _ in range(length)]
+        marked = [False for _ in range(length)]
+        
+        # Following is a typical code for the topological sort
+        for idx, node in enumerate(edges):
+            if node != -1:
+                indegree[node] += 1
+    
+        for i in range(length):
+            if not indegree[i]: dq.append(i)
+                
+        while dq:
+            node = dq.popleft()
+            # we remove the nodes we come across from the 
+            # candidates since any nodes in the cycle have other
+            # nodes pointing to them and thus have no chance of
+            # having an indegree of 0
+            candidates.remove(node)
+            if edges[node] != -1:
+                indegree[edges[node]] -= 1
+            if edges[node] != -1 and not indegree[edges[node]]:
+                dq.append(edges[node])
+        # if the candidates are empty, it means that there is no
+        # cycle in the graph 
+        if not candidates: return -1
+        max_cycle = 0
+        # Iterate over the nodes in the cycle only and skip 
+        # the ones that we have already examined
+        # use Floyd's algorithm to find the cycle length
+        for node in candidates:
+            if marked[node]: continue
+            
+            start = edges[node]
+            end = edges[edges[node]]
+            length = 1
+            marked[node] = marked[start] = marked[end] = True
+            while start != end:
+                start = edges[start]
+                end = edges[edges[end]]
+                marked[start] = marked[end] = True
+            start = node
+            while start != end:
+                start = edges[start]
+                end = edges[end]
+            first = start
+            
+            end = edges[first]
+            while end != first:
+                end = edges[end]
+                length += 1
+            max_cycle = max(max_cycle, length)
+        return max_cycle
