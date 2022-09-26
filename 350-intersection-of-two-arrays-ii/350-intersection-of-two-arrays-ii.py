edbf789e7f868013445d0c152caa10d3db8ff819
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-26 16:37:39 -0500

    Delete 350-intersection-of-two-arrays-ii directory

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1, nums2 = Counter(nums1), Counter(nums2)
-        t = []
-        for l in nums1:
-            if l in nums2:
-                for i in range(min(nums1[l], nums2[l])):
-                    t.append(l)
-        return t
