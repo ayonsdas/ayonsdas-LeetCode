c5061a31d55f3cc6fa68fa63acc1395f7217255a
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-26 18:53:36 -0500

    Time: 1023 ms (13.53%), Space: 15.3 MB (17.65%) - LeetHub

--- 1363-largest-multiple-of-three/1363-largest-multiple-of-three.py
+++ 1363-largest-multiple-of-three/1363-largest-multiple-of-three.py
@@ -1,33 +1,87 @@
 class Solution:
     def largestMultipleOfThree(self, digits: List[int]) -> str:
-        count = collections.Counter(digits)
-        
-        mod1 = count[1] + count[4] + count[7]
-        zeroMod1 = not mod1
-        mod1 %= 3
-        
-        mod2 = count[2] + count[5] + count[8]
-        zeroMod2 = not mod2
-        mod2 %= 3
-        
-        nums1, nums2 = (1, 4, 7), (2, 5, 8)
-        
-        def removeK(nums, k):
-            for i in nums:
-                rem = min(count[i], k)
-                count[i] -= rem
-                k -= rem  
-        
-        if mod1 == mod2:
-            removeK((), 0)
-        elif zeroMod1:
-            removeK(nums2, mod2)
-        elif zeroMod2:
-            removeK(nums1, mod1)
-        elif (mod1 - mod2) % 3 == 1:
-            removeK(nums1, 1)
+        digits = sorted(digits, reverse = True)
+        t = sum(digits)
+        if t == 0:
+            if len(digits) == 0:
+                return ""
+            return "0"
+        if t % 3 == 0:
+            return "".join(map(str, digits))
+        mod0 = []
+        mod1 = []
+        mod2 = []
+        for digit in digits:
+            if digit % 3 == 0:
+                mod0.append(digit)
+            elif digit % 3 == 1:
+                mod1.append(digit)
+            else:
+                mod2.append(digit)
+        if t % 3 == 1:
+            if len(mod1) >= 1:
+                t -= mod1[-1]
+                mod1.pop(-1)
+            elif len(mod2) >= 2:
+                t -= mod2[-1] + mod2[-2]
+                mod2.pop(-1)
+                mod2.pop(-1)
+            else:
+                return ""
         else:
-            removeK(nums2, 1)
-        
-        res = ''.join(str(digit) * count[digit] for digit in range(9, -1, -1))
-        return res if not res or res[0] != '0' else '0'
+            if len(mod2) >= 1:
+                t -= mod2[-1]
+                mod2.pop(-1)
+            elif len(mod1) >= 2:
+                t -= mod1[-1] + mod1[-2]
+                mod1.pop(-1)
+                mod1.pop(-1)
+            else:
+                return ""
+        x = ""
+        if t == 0:
+            if mod0:
+                return "0"
+            return ""
+        while mod0 or mod1 or mod2:
+            if not mod1 and not mod2:
+                x += str(mod0[0])
+                mod0 = mod0[1:]
+            elif not mod0 and not mod2:
+                x += str(mod1[0])
+                mod1 = mod1[1:]
+            elif not mod0 and not mod1:
+                x += str(mod2[0])
+                mod2 = mod2[1:]
+            elif not mod0:
+                if mod1[0] > mod2[0]:
+                    x += str(mod1[0])
+                    mod1 = mod1[1:]
+                else:
+                    x += str(mod2[0])
+                    mod2 = mod2[1:]
+            elif not mod1:
+                if mod0[0] > mod2[0]:
+                    x += str(mod0[0])
+                    mod0 = mod0[1:]
+                else:
+                    x += str(mod2[0])
+                    mod2 = mod2[1:]
+            elif not mod2:
+                if mod1[0] > mod0[0]:
+                    x += str(mod1[0])
+                    mod1 = mod1[1:]
+                else:
+                    x += str(mod0[0])
+                    mod0 = mod0[1:]
+            else:
+                if mod1[0] > mod2[0] and mod1[0] > mod0[0]:
+                    x += str(mod1[0])
+                    mod1 = mod1[1:]
+                elif mod0[0] > mod2[0] and mod0[0] > mod1[0]:
+                    x += str(mod0[0])
+                    mod0 = mod0[1:]
+                else:
+                    x += str(mod2[0])
+                    mod2 = mod2[1:]
+        return x
