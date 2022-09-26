181fc48759662ad4ea5ab8c93950b240e8ee6fe4
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-26 12:17:22 -0500

    Delete 1310-xor-queries-of-a-subarray directory

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        l = [0]
-        x = []
-        for i in range(len(arr)):
-            l.append(l[-1] ^ arr[i])
-        for query in queries:
-            x.append(l[query[1] + 1] ^ l[query[0]])
-        return x
