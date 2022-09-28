c559775e4217da59ba149ceac7d089f0b2b2a5b9
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-27 22:05:55 -0500

    Time: 2488 ms (33.04%), Space: 30.2 MB (74.32%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.py
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.py
@@ -4,10 +4,8 @@ class Solution:
         r = Counter(nums[1::2])
         s = sorted(l.keys(), key = lambda x : l[x], reverse = True)
         t = sorted(r.keys(), key = lambda x : r[x], reverse = True)
-        i = len(nums[::2])
-        j = len(nums[1::2])
         if len(t) == 0:
             return 0
         if s[0] != t[0]:
-            return i + j - l[s[0]] - r[t[0]]
-        return min(i + j - (l[s[1]] if len(s) > 1 else 0) - r[t[0]], i + j - (r[t[1]] if len(t) > 1 else 0) - l[s[0]])
+            return len(nums) - l[s[0]] - r[t[0]]
+        return min(len(nums) - (l[s[1]] if len(s) > 1 else 0) - r[t[0]], len(nums) - (r[t[1]] if len(t) > 1 else 0) - l[s[0]])
