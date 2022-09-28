cad3c51671b5904891c70cc19965f295ba56773e
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-27 22:04:30 -0500

    Time: 2808 ms (22.33%), Space: 30.1 MB (74.32%) - LeetHub

---
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        l = Counter(nums[::2])
+        r = Counter(nums[1::2])
+        s = sorted(l.keys(), key = lambda x : l[x], reverse = True)
+        t = sorted(r.keys(), key = lambda x : r[x], reverse = True)
+        i = len(nums[::2])
+        j = len(nums[1::2])
+        if len(t) == 0:
+            return 0
+        if s[0] != t[0]:
+            return i + j - l[s[0]] - r[t[0]]
+        return min(i + j - (l[s[1]] if len(s) > 1 else 0) - r[t[0]], i + j - (r[t[1]] if len(t) > 1 else 0) - l[s[0]])
