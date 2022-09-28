6094befa1bd9447a1ed3efc74bee953f098b363d
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-28 12:04:25 -0500

    Time: 230 ms (12.60%), Space: 14.3 MB (24.99%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,18 +1,25 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        x = []
-        z = 0
-        while nums1 or nums2:
-            if not nums1:
-                x.append(nums2.pop(-1))
-            elif not nums2:
-                x.append(nums1.pop(-1))
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        length = len(nums1) + len(nums2)
+        half = length // 2
+        l, r = 0, len(nums1) - 1
+        
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+            
+            l1 = nums1[i] if i >= 0 else float('-inf')
+            r1 = nums1[i + 1] if i < len(nums1) - 1 else float('inf')
+            l2 = nums2[j] if j >= 0 else float('-inf')
+            r2 = nums2[j + 1] if j < len(nums2) - 1 else float('inf')
+            if l1 <= r2 and l2 <= r1:
+                if length % 2:
+                    return float(min(r1, r2))
+                return (min(r1, r2) + max(l1, l2)) / 2
+            if l1 > r2:
+                r = i - 1
             else:
-                if nums1[-1] > nums2[-1]:
-                    x.append(nums1.pop(-1))
-                else:
-                    x.append(nums2.pop(-1))
-            z += 1
-        if z & 1:
-            return float(x[z // 2])
-        return float(x[z // 2] + x[z // 2 - 1]) / 2
+                l = i + 1
