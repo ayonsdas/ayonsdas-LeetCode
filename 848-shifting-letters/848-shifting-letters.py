18cf465cbb513887e556e357d71a417d48c23e83
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-29 14:20:38 -0500

    Time: 1188 ms (68.72%), Space: 27.5 MB (48.10%) - LeetHub

---
+++ 848-shifting-letters/848-shifting-letters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        chars = [chr(i + 97) for i in range(26)]
+        s = [ord(c) for c in s]
+        l = [0]
+        for shift in shifts[::-1]:
+            l.append(l[-1] + shift)
+            l[-1] %= 26
+        for i in range(len(s)):
+            s[i] += l[~i]
+            if s[i] > 122:
+                s[i] -= 26
+        return "".join([chr(c) for c in s])
