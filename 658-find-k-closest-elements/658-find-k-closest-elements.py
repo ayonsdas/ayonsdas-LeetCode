12797cfe87966eea8a719d4e605454b88214cc50
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-28 19:11:08 -0500

    Time: 284 ms (98.94%), Space: 15.4 MB (80.93%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,12 +1,18 @@
-from heapq import heappop, heappush
-
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l = []
-        for y in arr:
-            if len(l) == k:
-                if -abs(x - y) > l[0][0]:
-                    heappushpop(l, (-abs(x - y), y))
+        # use two mid pointes method
+        left, right = 0, len(arr) - k
+        
+        while left < right:
+            mid = (left + right) // 2
+            # mid + k must not out of range
+            if abs(arr[mid] - x) > abs(arr[mid + k]  - x):
+                left = mid + 1
+            # if All duplicate and the first one is large than x
+            # arr[mid] < x need to filter out this kind of case: [1,1,2,2,2,2,2,2,2,2] 3 1
+            elif arr[mid] == arr[mid + k] and arr[mid] < x:
+                left = mid + 1
             else:
-                heappush(l, (-abs(x - y), y))
-        return sorted([x[1] for x in l])
+                right = mid
+            
+        return arr[left: left + k]
