417c91c780f55458a2ea84c41ca07f73417d039b
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-28 22:03:11 -0500

    Time: 68 ms (28.67%), Space: 13.9 MB (61.68%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        s = set()
+        while True:
+            if n == 1:
+                return True
+            if n in s:
+                return False
+            s.add(n)
+            n = self.modify(n)
+            
+    def modify(self, n: int) -> int:
+        t = 0
+        while n:
+            t += (n % 10) ** 2
+            n //= 10
+        return t
