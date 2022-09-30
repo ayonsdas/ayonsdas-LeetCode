a3bd6bcf3ee336c3fbe4667d6c4fd7e8fd23013d
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-30 14:21:39 -0500

    Time: 96 ms (14.67%), Space: 14 MB (75.27%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePathsWithObstacles(self, o: List[List[int]]) -> int:
+        if len(o) == 1 and len(o[0]) == 1:
+            return 1 - o[0][0]
+        dp = [[0 for x in o[0]] for y in o]
+        dp[0][0] = 1 - o[0][0]
+        for i in range(len(o)):
+            for j in range(len(o[0])):
+                if o[i][j] != 1 and (i != 0 or j != 0):
+                    dp[i][j] = (dp[i - 1][j] if i > 0 else 0) + (dp[i][j - 1] if j > 0 else 0)
+        return dp[-1][-1]
