91bab4a2235934cd819f41ce89c490ae6b9858d5
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-09-30 14:28:18 -0500

    Delete 1014-best-sightseeing-pair directory

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        r = 0
-        c = 0
-        for i, a in enumerate(values):
-            r = max(r, c + a)
-            c = max(a, c) - 1
-        return r
