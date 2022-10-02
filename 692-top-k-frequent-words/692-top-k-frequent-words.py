0baafa141fcc581424e677046dd77674accf0b25
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-02 13:56:05 -0500

    Delete 692-top-k-frequent-words directory

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        r = Counter(words)
-        l = list(r.keys())
-        l.sort()
-        l = sorted(l, key = lambda x: -r[x])
-        return l[:k]
