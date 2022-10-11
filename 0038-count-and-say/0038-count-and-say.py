8bc5518833240a4e88f8f368d65fd09c7e0464a5
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-11 16:23:19 -0500

    Time: 122 ms (9.91%), Space: 14 MB (27.96%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -5,16 +5,15 @@ class Solution:
         t = self.countAndSay(n - 1)
         c = t[0]
         t = t[1:]
-        x = []
+        x = ""
         n = 1
         while t:
             if t[0] == c:
                 n += 1
             else:
-                x.append([n, c])
+                x += str(n) + str(c)
                 c = t[0]
                 n = 1
             t = t[1:]
-        x.append([n, c])
-        return "".join([str(a[0]) + str(a[1]) for a in x])
-        print(x)
+        x += str(n) + str(c)
+        return x
