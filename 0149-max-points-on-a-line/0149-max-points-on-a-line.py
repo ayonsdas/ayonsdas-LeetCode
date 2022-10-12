9a490daf69fba3a5f3805e8a8c1aa0b175ba283a
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-12 17:40:54 -0500

    Time: 94 ms (91.73%), Space: 14 MB (48.29%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,23 +1,17 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         m = 1
-        for i in range(len(points)):
-            for j in range(i + 1, len(points)):
-                c = 2
-                rise = points[j][1] - points[i][1]
-                run = points[j][0] - points[i][0]
-                u = (run != 0)
-                b = 0
-                if u:
-                    b = points[j][1] * run - points[j][0] * rise
-                for k in range(len(points)):
-                    if k != i and k != j:
-                        if u:
-                            if points[k][0] * rise + b == points[k][1] * run:
-                                c += 1
-                        else:
-                            if points[k][0] == points[i][0]:
-                                c += 1
         
-                m = max(m, c)
+        def sl(i: int, j: int):
+            x1, y1 = points[i]
+            x2, y2 = points[j]
+            if x1 - x2 == 0:
+                return float('inf')
+            return (y2 - y1) / (x2 - x1)
+        
+        for i in range(len(points)):
+            s = defaultdict(int)
+            for j in range(i + 1, len(points)):
+                s[sl(i, j)] += 1
+                m = max(m, s[sl(i, j)] + 1)
         return m
