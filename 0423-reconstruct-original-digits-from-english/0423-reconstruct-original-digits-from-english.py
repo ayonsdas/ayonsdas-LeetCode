154588792588e29545b8f94f656a8cb25085fe89
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-21 16:48:08 -0500

    Time: 239 ms (13.22%), Space: 14.3 MB (77.22%) - LeetHub

--- 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.py
+++ 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.py
@@ -5,12 +5,9 @@ class Solution:
         while s['z'] > 0:
             t[0] += 1
             s['z'] -= 1
-            s['e'] -= 1
-            s['r'] -= 1
             s['o'] -= 1
         while s['g'] > 0:
             t[8] += 1
-            s['e'] -= 1
             s['i'] -= 1
             s['g'] -= 1
             s['h'] -= 1
@@ -19,11 +16,8 @@ class Solution:
             t[3] += 1
             s['t'] -= 1
             s['h'] -= 1
-            s['r'] -= 1
-            s['e'] -= 2
         while s['x'] > 0:
             t[6] += 1
-            s['s'] -= 1
             s['i'] -= 1
             s['x'] -= 1
         while s['u'] > 0:
@@ -37,18 +31,12 @@ class Solution:
             s['f'] -= 1
             s['i'] -= 1
             s['v'] -= 1
-            s['e'] -= 1
         while s['v'] > 0:
             t[7] += 1
-            s['s'] -= 1
             s['v'] -= 1
-            s['e'] -= 2
-            s['n'] -= 1
         while s['i'] > 0:
             t[9] += 1
-            s['n'] -= 2
             s['i'] -= 1
-            s['e'] -= 1
         while s['t'] > 0:
             t[2] += 1
             s['t'] -= 1
