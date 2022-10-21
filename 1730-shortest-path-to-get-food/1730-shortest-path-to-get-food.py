0086a2c9e212f057604efe0eeb20c16bac285af1
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-21 17:29:56 -0500

    Time: 1586 ms (14.65%), Space: 20.1 MB (46.83%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getFood(self, g: List[List[str]]) -> int:
+        x = y = 0
+        lg = len(g)
+        lg2 = len(g[0])
+        i = 0
+        while i < lg:
+            j = 0
+            while j < lg2:
+                if g[i][j] == "*":
+                    x, y = i, j
+                    break
+                j += 1
+            else:
+                i += 1
+                continue
+            break
+        v = set()
+        d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        q = [(x, y, 0)]
+        while q:
+            x, y, l = q.pop(0)
+            if x >= 0 and x < lg and y >= 0 and y < lg2 and not(x, y) in v and g[x][y] != "X":
+                if g[x][y] == "#":
+                    return l
+                for dx, dy in d:
+                    q.append((x + dx, y + dy, l + 1))
+                v.add((x, y))
+        return -1
