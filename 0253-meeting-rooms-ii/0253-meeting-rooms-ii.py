a30996c2eb19ec33f8705fa3bff87743aabc705d
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-22 15:30:47 -0500

    Time: 180 ms (33.54%), Space: 17.4 MB (83.58%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,12 @@
+from heapq import heappush, heapreplace
+
+class Solution:
+    def minMeetingRooms(self, intervals):
+        intervals.sort(key=lambda x : x[0])
+        heap = []
+        for i in intervals:
+            if heap and i[0] >= heap[0]:
+                heapreplace(heap, i[1])
+            else:
+                heappush(heap, i[1])
+        return len(heap)
