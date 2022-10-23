f361fc5661a4ea0427b8fff19f3ed97f1e57e596
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 14:36:32 -0500

    Time: 2024 ms (5.74%), Space: 17.8 MB (21.18%) - LeetHub

--- 0475-heaters/0475-heaters.py
+++ 0475-heaters/0475-heaters.py
@@ -20,23 +20,7 @@ class Solution:
                     r = m - 1
             if heaters[x1] == h:
                 continue
-            l = 0
-            r = len(heaters) - 1
-            x2 = -1
-            while l <= r:
-                m = (l + r) // 2
-                if heaters[m] == h or heaters[m] > h and (m == 0 or heaters[m - 1] < h):
-                    x2 = m
-                    break
-                elif m > 0 and heaters[m - 1] == h:
-                    x1 = m - 1
-                    break
-                elif heaters[m] < h:
-                    l = m + 1
-                else:
-                    r = m - 1
-            if heaters[x2] == h:
-                continue
+            x2 = (x1 + 1) if x1 != len(heaters) - 1 else -1
             a = (h - heaters[x1]) if x1 != -1 else float('inf')
             b = (heaters[x2] - h) if x2 != -1 else float('inf')
             md = max(md, min(a, b))
