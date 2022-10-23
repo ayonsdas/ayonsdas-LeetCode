deed71e7421c3062b5490399849d0b0b2f69c52d
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 16:21:14 -0500

    Time: 40 ms (87.94%), Space: 13.8 MB (60.29%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.py
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -2,13 +2,8 @@ from itertools import permutations
 
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        a = 1
-        b = set()
-        for i in range(30):
-            b.add(a)
-            a *= 2
-        t = set(["".join(a) if a[0] != '0' else '9999' for a in permutations([char for char in str(n)])])
-        for x in b:
-            if str(x) in t:
-                return True
-        return False
+        b = []
+        for i in range(31):
+            b.append(Counter(list(str(1<<i))))
+        t = Counter(list(str(n)))
+        return t in b
