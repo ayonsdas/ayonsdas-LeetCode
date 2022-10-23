0d8c912639bb3ecfe13be4512ca1bc8acbfe8afd
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 15:08:35 -0500

    Delete 0034-find-first-and-last-position-of-element-in-sorted-array directory

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [bisect.bisect_left(nums, target), bisect.bisect_right(nums, target) - 1] if target in set(nums) else [-1, -1]
