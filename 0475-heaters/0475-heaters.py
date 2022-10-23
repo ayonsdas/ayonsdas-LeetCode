c5000b7c78caafd0e355751d51543929aa723384
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 14:48:55 -0500

    Time: 498 ms (72.22%), Space: 17.7 MB (41.11%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters = sorted(heaters)
+        md = 0
+        for h in houses:
+            l = 0
+            r = len(heaters) - 1
+            x1 = -1
+            while l <= r:
+                m = (l + r) // 2
+                if heaters[m] == h or heaters[m] < h and (m == len(heaters) - 1 or heaters[m + 1] >= h):
+                    x1 = m
+                    break
+                elif heaters[m] < h:
+                    l = m + 1
+                else:
+                    r = m - 1
+            if heaters[x1] == h or m < len(heaters) - 1 and heaters[m + 1] == h:
+                continue
+            x2 = (x1 + 1)
+            a = (h - heaters[x1]) if x1 != -1 else float('inf')
+            b = (heaters[x2] - h) if x2 != len(heaters) else float('inf')
+            md = max(md, min(a, b))
+        return md
