5b00cb531e12f937fbd11cbd195680cb2b9726ed
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 18:15:05 -0500

    Time: 1524 ms (72.33%), Space: 36.8 MB (63.36%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -4,4 +4,4 @@ class Solution:
         for i in range(len(nums)):
             for j in range(len(nums[i])):
                 x.append((i, j, nums[i][j]))
-        return [a[2] for a in sorted(sorted(x, key = lambda a : a[1]), key = lambda a : a[0] + a[1])]
+        return [a[2] for a in sorted(sorted(x, key = lambda a : a[1]), key = lambda a : a[0] + a[1])] 
