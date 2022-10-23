279caad3ffecf53ea28a4cd9b05adaa57ac2914b
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 12:31:33 -0500

    Time: 500 ms (91.23%), Space: 15.4 MB (70.90%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalHammingDistance(self, nums: List[int]) -> int:
+        a = 0
+        b = 1
+        for i in range(30):
+            c = 0
+            d = 0
+            for i in range(len(nums)):
+                if nums[i] & b:
+                    c += 1
+                else:
+                    d += 1
+            a += c * d
+            b <<= 1
+        return a
