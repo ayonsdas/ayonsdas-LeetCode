596d17f668823418940b6e0496580d8f4e258993
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 14:58:58 -0500

    Time: 696 ms (47.15%), Space: 17.6 MB (41.11%) - LeetHub

--- 0475-heaters/0475-heaters.py
+++ 0475-heaters/0475-heaters.py
@@ -5,20 +5,11 @@ class Solution:
         for h in houses:
             l = 0
             r = len(heaters) - 1
-            x1 = -1
-            while l <= r:
-                m = (l + r) // 2
-                if heaters[m] == h or heaters[m] < h and (m == len(heaters) - 1 or heaters[m + 1] >= h):
-                    x1 = m
-                    break
-                elif heaters[m] < h:
-                    l = m + 1
-                else:
-                    r = m - 1
-            if heaters[x1] == h or m < len(heaters) - 1 and heaters[m + 1] == h:
-                continue
-            x2 = (x1 + 1)
-            a = (h - heaters[x1]) if x1 != -1 else float('inf')
-            b = (heaters[x2] - h) if x2 != len(heaters) else float('inf')
-            md = max(md, min(a, b))
+            x1 = bisect.bisect(heaters, h)
+            if x1 == 0:
+                md = max(md, heaters[0] - h)
+            elif x1 == len(heaters):
+                md = max(md, h - heaters[-1])
+            else:
+                md = max(md, min(h - heaters[x1 - 1], heaters[x1] - h))
         return md
