2008568cd3a2dcc4fcf4290d05cbf68936dbb9ef
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 15:29:33 -0500

    Time: 949 ms (17.27%), Space: 14.1 MB (69.25%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,15 +1,15 @@
+dirs = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
 class Solution:
     def knightProbability(self, n: int, k: int, r: int, c: int) -> float:
-        
-        @lru_cache(None)
-        def dp(mL, cR, cC):
-            if cR < 0 or cR >= n or cC < 0 or cC >= n:
-                return 0
-            if mL == 0:
-                return 1
-            m = [(-2, 1), (-1, 2), (2, -1), (1, -2), (1, 2), (2, 1), (-1, -2), (-2, -1)]
-            l = 0
-            for dx, dy in m:
-                l += dp(mL - 1, cR + dx, cC + dy)
-            return l / 8
-        return dp(k, r, c)
+        dp = [[0] * n for _ in range(n)]
+        dp[r][c] = 1
+        for _ in range(k):
+            dp2 = [[0] * n for _ in range(n)]
+            for r, row in enumerate(dp):
+                for c, val in enumerate(row):
+                    for dr, dc in dirs:
+                        if 0 <= r + dr < n and 0 <= c + dc < n:
+                            dp2[r + dr][c + dc] += val / 8.0
+            dp = dp2
+        return sum(map(sum, dp))
