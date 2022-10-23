21cb1986df49a4e9ca03e3560323a4015a155e37
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 11:43:40 -0500

    Delete 0442-find-all-duplicates-in-an-array directory

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        a = []
-        for n in nums:
-            if nums[abs(n) - 1] > 0:
-                nums[abs(n) - 1] *= -1
-            else:
-                a.append(abs(n))
-        return a
