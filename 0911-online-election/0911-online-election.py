2af781cbc117f868dab9bbd634e017e478337edc
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 16:40:41 -0500

    Time: 767 ms (86.84%), Space: 19.7 MB (95.14%) - LeetHub

--- 0911-online-election/0911-online-election.py
+++ 0911-online-election/0911-online-election.py
@@ -2,7 +2,8 @@ class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
         self.times = times
-        x = [0 for _ in range(5001)]
+        a = max(persons)
+        x = [0 for _ in range(a + 1)]
         self.l = []
         leader = -1
         mScore = 0
