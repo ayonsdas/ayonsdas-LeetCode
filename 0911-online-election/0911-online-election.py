6f4328d3e861d318a85cdb995969326278c0ec20
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-23 16:38:31 -0500

    Time: 1640 ms (39.07%), Space: 20 MB (29.96%) - LeetHub

--- 0911-online-election/0911-online-election.py
+++ 0911-online-election/0911-online-election.py
@@ -3,17 +3,20 @@ class TopVotedCandidate:
     def __init__(self, persons: List[int], times: List[int]):
         self.times = times
         a = max(persons)
-        x = [0 for _ in range(a + 1)]
-        self.l = []
+        x = {}
+        self.l = [-1 for _ in range(len(times))] 
         leader = -1
         mScore = 0
+        i = 0
         for p in persons:
+            if not p in x:
+                x[p] = 0
             x[p] += 1
             if x[p] >= mScore:
                 mScore = x[p]
                 leader = p
-            self.l.append(leader)
-        print(self.l)
+            self.l[i] = leader
+            i += 1
 
     def q(self, t: int) -> int:
         return self.l[bisect.bisect_right(self.times, t) - 1]
