7ad34ec58046365777a6f55283419f6049b95944
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-24 15:47:42 -0500

    Time: 4036 ms (15.66%), Space: 25.2 MB (18.54%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,10 +1,10 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.x = [[-1 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+        self.x = matrix
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                self.x[i][j] = matrix[i][j] + (self.x[i - 1][j] if i > 0 else 0) + (self.x[i][j - 1] if j > 0 else 0) - (self.x[i - 1][j - 1] if (i > 0 and j > 0) else 0)
+                self.x[i][j] += (self.x[i - 1][j] if i > 0 else 0) + (self.x[i][j - 1] if j > 0 else 0) - (self.x[i - 1][j - 1] if (i > 0 and j > 0) else 0)
         print(self.x)
 
     def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
