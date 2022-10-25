7f5742f0f7e01d7160ab6fe1137cf1d1b9f178d8
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-25 17:18:27 -0500

    Time: 76 ms (30.57%), Space: 14 MB (37.19%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        sol = []
+        self.finder([], s, sol)
+        return sol
+    
+    def finder(self, curr: List[str], s: str, sol: List[str]) -> None:
+        
+        if not s:
+            if len(curr) == 4:
+                sol.append(".".join(curr))
+            return
+        if s == "0":
+            curr.append(s[0])
+            self.finder(curr, s[1:], sol)
+            curr.pop(-1)
+            return
+        curr.append(s[0])
+        self.finder(curr, s[1:], sol)
+        curr.pop(-1)
+        if s[0] == "0" or len(s) == 1:
+            return
+        curr.append(s[:2])
+        self.finder(curr, s[2:], sol)
+        curr.pop(-1)
+        if len(s) >= 3 and int(s[:3]) <= 255:
+            curr.append(s[:3])
+            self.finder(curr, s[3:], sol)
+            curr.pop(-1)
