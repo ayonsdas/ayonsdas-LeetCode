f61af94810f0bd71b32f5e235f41533f68aed2de
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-25 19:22:12 -0500

    Time: 50 ms (70.92%), Space: 108.8 MB (71.64%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        Set<Integer> s = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            int r = nums[i] % k;
+            if(i == 1) {
+                s.add(0);
+            }
+            else if(i > 1) {
+                s.add(a[i - 2]);
+            }
+            if(i != 0) {
+                r = (r + a[i - 1]) % k;
+            }
+            if(s.contains(r))
+                return true;
+            a[i] = r;
+        }
+        return false;
+    }
+}
