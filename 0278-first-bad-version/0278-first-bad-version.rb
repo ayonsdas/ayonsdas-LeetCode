78201c5da75ed91aa45a688f4936ee872f176f7a
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-26 17:39:03 -0500

    Time: 71 ms (96.04%), Space: 210.9 MB (33.04%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.rb
@@ -0,0 +1,28 @@
+# The is_bad_version API is already defined for you.
+# @param {Integer} version
+# @return {boolean} whether the version is bad
+# def is_bad_version(version):
+
+# @param {Integer} n
+# @return {Integer}
+
+def first_bad_version(n)
+    l, r = 1, n
+    while l < r
+        m = (l + r) / 2
+        if r - l >= 2
+            if is_bad_version(m)
+                r = m
+            else
+                l = m
+            end
+        else
+            if is_bad_version(m)
+                return m
+            else
+                return m + 1
+            end
+        end
+    end
+    return l
+end
