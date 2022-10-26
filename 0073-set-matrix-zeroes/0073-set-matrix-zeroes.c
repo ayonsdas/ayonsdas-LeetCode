5683952bfa3f4f1cac29289dd4f7e742a38b951a
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-26 18:17:05 -0500

    Time: 50 ms (92.59%), Space: 10.9 MB (92.06%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.c
@@ -0,0 +1,68 @@
+
+
+void setZeroes(int** mx, int matrixSize, int* matrixColSize)
+{
+    bool r = false;
+    bool c = false;
+    for(int i = 0; i < matrixSize; i++)
+    {
+        if(mx[i][0] == 0)
+        {
+            c = true;
+            break;
+        }
+    }
+    for(int j = 0; j < *matrixColSize; j++)
+    {
+        if(mx[0][j] == 0)
+        {
+            r = true;
+            break;
+        }
+    }
+    for(int i = 1; i < matrixSize; i++)
+    {
+        for(int j = 1; j < *matrixColSize; j++)
+        {
+            if(mx[i][j] == 0)
+            {
+                mx[0][j] = 0;
+                mx[i][0] = 0;
+            }
+        }
+    }
+    for(int i = 1; i < matrixSize; i++)
+    {
+        if(mx[i][0] == 0)
+        {
+            for(int j = 1; j < *matrixColSize; j++)
+            {
+                mx[i][j] = 0;
+            }
+        }
+    }
+    for(int j = 1; j < *matrixColSize; j++)
+    {
+        if(mx[0][j] == 0)
+        {
+            for(int i = 1; i < matrixSize; i++)
+            {
+                mx[i][j] = 0;
+            }
+        }
+    }
+    if(r)
+    {
+        for(int j = 0; j < *matrixColSize; j++)
+        {
+            mx[0][j] = 0;
+        }
+    }
+    if(c)
+    {
+        for(int i = 0; i < matrixSize; i++)
+        {
+            mx[i][0] = 0;
+        }
+    }
+}
