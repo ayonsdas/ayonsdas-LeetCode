ff38e855d52c3387c181e50eb778038a427d8899
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-27 14:09:05 -0500

    Time: 59 ms (97.17%), Space: 42.4 MB (18.82%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums)
+{
+    return search(nums, 0, nums.length - 1);
+};
+
+var search = function(nums, l, r)
+{
+    if (l == r)
+        return l;
+    var mid = l + Math.floor((r - l) / 2);
+    if(nums[mid] > nums[mid + 1])
+        return search(nums, l, mid);
+    return search(nums, mid + 1, r);
+};
