f57013a0587515b13c504391fa2807fe08de7439
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-27 14:38:09 -0500

    Time: 1 ms (63.29%), Space: 2 MB (100.00%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.rs
@@ -0,0 +1,38 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        
+        let mut returnable = vec![];
+        fn traverse(node: Option<Rc<RefCell<TreeNode>>>, r: &mut Vec<i32>)
+        {
+            if let Some(n) = node
+            {
+                r.push(n.borrow().val);
+                traverse(n.borrow().left.clone(), r);
+                traverse(n.borrow().right.clone(), r);
+            }
+        }
+        traverse(root, &mut returnable);
+        return returnable;
+        
+    }
+}
