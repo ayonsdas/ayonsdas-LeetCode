24d672743a05a8b73444e18dad8ee7c34c06e35b
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-27 12:21:39 -0500

    Time: 3 ms (23.33%), Space: 2 MB (90.95%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn hammingWeight (n: u32) -> i32 {
+        let mut c : i32 = 0;
+        let mut x : u32 = n;
+        while x > 0
+        {
+            if (x & 1 != 0)
+            {
+                c += 1;
+            }
+            x >>= 1;
+        }
+        return c;
+    }
+}
