8edd0cc451272a62da240b730f036b5acb6c41cb
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-27 22:29:29 -0500

    Time: 47 ms (78.87%), Space: 22.4 MB (23.94%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.php
+++ 0042-trapping-rain-water/0042-trapping-rain-water.php
@@ -22,22 +22,4 @@ class Solution {
         }
         return $x;
     }
-}
-
-/**
-# @param {Integer[]} height
-# @return {Integer}
-def trap(height)
-    heightL = [0]
-    heightR = [0]
-    for i in 1..height.length() - 1
-        heightL.push([heightL[-1], height[i - 1]].max())
-        heightR.unshift([heightR[0], height[-i]].max())
-    end
-    x = 0
-    for i in 0..height.length() - 1
-        x += [0, [heightL[i], heightR[i]].min() - height[i]].max()
-    end
-    return x
-end
-*/
+} 
