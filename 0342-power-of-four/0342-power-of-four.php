33cf86f247766f6cec5055a284313d285ddb30f8
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-29 13:11:16 -0500

    Time: 15 ms (66.67%), Space: 19.8 MB (5.56%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.php
@@ -0,0 +1,14 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isPowerOfFour($n) {
+        if($n > 0)
+        {
+            return (fmod(log($n, 4.0), 1.0) == 0.0);
+        }
+        return false;
+    }
+}
