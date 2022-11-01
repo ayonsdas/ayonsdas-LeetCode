92b609bb83713f2329a93803aad4a295cd7f0c60
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-10-31 21:39:27 -0500

    Time: 238 ms (87.69%), Space: 24.3 MB (19.03%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        
+        s = stones[0]
+        
+        t = stones[-1]
+        
+        stones = set(stones)
+        
+        @lru_cache(None)
+        def dp(p: int, pre: int) -> bool:
+            
+            
+            if not p in stones or p > t:
+                return False
+            
+            if p == t:
+                return True
+            
+            return (dp(p + pre - 1, pre - 1) if pre > 1 else False) or dp(p + pre, pre) or dp(p + pre + 1, pre + 1)
+            
+        
+        return dp(s + 1, 1)
