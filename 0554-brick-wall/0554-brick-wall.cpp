bb3e001db0cd072144c857fee0da3280fcd2fef7
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-01 20:53:33 -0500

    Time: 121 ms (19.31%), Space: 21.6 MB (95.13%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-        map<int, int> m;
+        unordered_map<int, int> m;
         for(int i = 0; i < wall.size(); i++)
         {
             int k = 0;
