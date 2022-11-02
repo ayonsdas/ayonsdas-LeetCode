0c5f1ffd2313c6c28649fb19be11b02ed773342f
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-01 19:33:37 -0500

    Time: 256 ms (50.00%), Space: 212.8 MB (50.00%) - LeetHub

---
+++ 0554-brick-wall/0554-brick-wall.rb
@@ -0,0 +1,19 @@
+# @param {Integer[][]} wall
+# @return {Integer}
+def least_bricks(wall)
+    stuff = Hash.new()
+    for i in 0..wall.length()-1
+        k = 0
+        for j in 0..wall[i].length()-2
+            k += wall[i][j]
+            if not stuff.key?(k)
+                stuff[k] = 0
+            end
+            stuff[k] = stuff[k] + 1
+        end
+    end
+    if stuff.empty?()
+        return wall.length()
+    end
+    return wall.length() - stuff[stuff.max_by{|k, v| v}[0]]
+end
