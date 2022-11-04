8008224b28aeba1cda48f9bc8d9975c1d0f88b82
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-03 22:17:18 -0500

    Time: 195 ms (37.37%), Space: 27.1 MB (74.24%) - LeetHub

--- 0478-generate-random-point-in-a-circle/0478-generate-random-point-in-a-circle.cpp
+++ 0478-generate-random-point-in-a-circle/0478-generate-random-point-in-a-circle.cpp
@@ -14,11 +14,8 @@ public:
     }
     
     vector<double> randPoint() {
-        vector<double> a;
         double rR = sqrt(((double) rand() / (double) RAND_MAX)) * this->r;
         double tR = ((double) rand() / (double) RAND_MAX) * 2 * M_PI;
-        a.push_back(x + rR * cos(tR));
-        a.push_back(y + rR * sin(tR));
-        return a;
+        return vector<double>{x + rR * cos(tR), y + rR * sin(tR)};
     }
 }; 
