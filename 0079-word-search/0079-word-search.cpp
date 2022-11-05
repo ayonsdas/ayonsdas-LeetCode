03c92fe4739e6e7457d529ceb90680ab6a77d1c1
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-05 18:41:09 -0500

    Time: 683 ms (53.47%), Space: 8 MB (79.49%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word)
+    {
+        for(int i = 0; i < board.size(); i++)
+            for(int j = 0; j < board[i].size(); j++)
+                if(dfs(board, i, j, word, 0))
+                    return true;
+        return false;
+    }
+    
+    bool dfs(vector<vector<char>>& board, int x, int y, string word, int p)
+    {
+        if(p == word.size())
+            return true;
+        
+        if(x < 0 || y < 0 || x >= board.size() || y >= board[0].size() || board[x][y] != word[p])
+            return false;
+        
+        char c = board[x][y];
+        board[x][y] = '#';
+        
+        if(dfs(board, x - 1, y, word, p + 1))
+            return true;
+        
+        if(dfs(board, x + 1, y, word, p + 1))
+            return true;
+        
+        if(dfs(board, x, y - 1, word, p + 1))
+            return true;
+        
+        if(dfs(board, x, y + 1, word, p + 1))
+            return true;
+        
+        board[x][y] = c;
+        
+        return false;
+    }
+};
