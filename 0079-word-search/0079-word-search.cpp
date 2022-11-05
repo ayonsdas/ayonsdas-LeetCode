2fadf2245fe4fb10dc915864b2eb1a12442200c9
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-05 18:44:56 -0500

    Time: 302 ms (90.35%), Space: 8.1 MB (47.28%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word)
     {
+        unordered_map<char, int> m;
+        for(char c : word)
+            m[c]++;
+        
+        unordered_map<char, int> m2;
+        for(vector<char>& bo : board)
+            for(char c : bo)
+                m2[c]++;
+        
+        for(auto pair : m)
+            if(m[pair.first] > m2[pair.first])
+                return false;
+        
         for(int i = 0; i < board.size(); i++)
             for(int j = 0; j < board[i].size(); j++)
                 if(dfs(board, i, j, word, 0))
