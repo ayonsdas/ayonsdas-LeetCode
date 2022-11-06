6d4165f4147be12489b69f67f2c76c928a7a573d
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-05 19:05:15 -0500

    Time: 7 ms (63.22%), Space: 10.8 MB (52.30%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k == 0)
+            return s;
+        if(k > 1)
+        {
+            sort(s.begin(), s.end());
+            return s;
+        }
+        string st = s;
+        for(int i = 1; i < s.size(); i++)
+        {
+            string p = s.substr(i) + s.substr(0, i);
+            if(st > p)
+                st = p;
+        }
+        return st;
+    }
+};
