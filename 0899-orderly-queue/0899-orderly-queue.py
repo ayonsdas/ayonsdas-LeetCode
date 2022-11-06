78a6ca1f5150dcae65cf0be6dd211863db817198
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-05 19:08:08 -0500

    Time: 37 ms (94.05%), Space: 13.8 MB (72.62%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -0,0 +1,11 @@
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k == 0:
+            return s
+        if k > 1:
+            return "".join(sorted(s))
+        st = s
+        for i in range(1, len(s)):
+            st = min(st, s[i:] + s[0:i])
+            
+        return st
