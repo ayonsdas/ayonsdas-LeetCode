27d3df0f154f07853bb16595d6745de9c66dbbfa
ayonsdas <114185886+ayonsdas@users.noreply.github.com>
2022-11-17 20:58:19 -0600

    Time: 5 ms (66.67%), Space: 13.7 MB (50.88%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func isUgly(_ n: Int) -> Bool {
+        if(n <= 0) {
+            return false;
+        }
+        var x = n;
+        while(x % 2 == 0) {
+            x /= 2;
+        }
+        while(x % 3 == 0) {
+            x /= 3;
+        }
+        while(x % 5 == 0) {
+            x /= 5;
+        }
+        return x == 1;
+    }
+}
